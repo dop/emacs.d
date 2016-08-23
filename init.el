152a89c82e4e54fb79f5b589ff2704433cb1c648
Donatas Petrauskas <donatas.petr@gmail.com>
2016-08-23 16:35:01 +0300

    dabbrev before abbrevs.

--- init.el
+++ init.el
@@ -834,10 +834,10 @@ of code to whatever theme I'm using's background"
   (setq hippie-expand-try-functions-list
         '(try-complete-file-name-partially
           try-complete-file-name
-          try-expand-all-abbrevs
           try-expand-dabbrev
           try-expand-dabbrev-all-buffers
           try-expand-dabbrev-from-kill
+          try-expand-all-abbrevs
           try-complete-lisp-symbol-partially
           try-complete-lisp-symbol
           try-expand-list
