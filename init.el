b1da917c0a0472ac0b629bacd464fecc5789ebc0
Donatas Petrauskas <donatas@Donatass-Air.lan>
2017-04-07 21:21:21 +0300

    :ensure all the things.

--- init.el
+++ init.el
@@ -21,6 +21,11 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(require 'use-package)
+
+(use-package f :ensure t)
+(use-package dash :ensure t)
+
 (require 'subr-x)
 (require 'f)
 (require 'dash)
@@ -31,9 +36,6 @@
 
 (when (fboundp 'mac-auto-operator-composition-mode)
   (mac-auto-operator-composition-mode t))
-
-(setq insert-directory-program "/usr/local/bin/gls")
-(setq dired-listing-switches "-alGhF --group-directories-first")
 
 (setenv "PAGER" "cat")
 (setq comint-input-ignoredups t)
@@ -111,7 +113,7 @@
 (column-number-mode t)
 (electric-pair-mode t)
 
-(show-paren-mode 1)
+(show-paren-mode t)
 (setq show-paren-style 'parenthesis)
 
 (setq require-final-newline t
@@ -140,7 +142,7 @@
 ;; increate text by 1
 (setq text-scale-mode-step 1.1)
 
-(add-hook 'post-command-hook 'set-cursor-according-to-mode t)
+;; (add-hook 'post-command-hook 'set-cursor-according-to-mode t)
 ;; (defun set-cursor-according-to-mode () (setq cursor-type 'box))
 ;; (remove-hook 'post-command-hook 'set-cursor-according-to-mode)
 
@@ -151,31 +153,17 @@
 (add-hook 'html-mode-hook #'turn-on-show-trailing-whitespace)
 (add-hook 'css-mode-hook #'turn-on-show-trailing-whitespace)
 
-(require 'use-package)
-
 (require 'dp-window)
 (require 'dp-mode-line)
 (require 'dp-key-bindings)
-
-;; (require 'quelpa nil t)
-;; (require 'quelpa-use-package)
 
 (use-package paradox
   :ensure t
   :commands (paradox-list-packages))
 
-(defalias 'list-packages 'paradox-list-packages)
-(defalias 'package-list-packages 'paradox-list-packages)
-
-;; (use-package server
-;;   :init (unless (server-running-p) (server-start)))
-
-(use-package zerodark
+(use-package zenburn-theme
   :disabled t
-  :init (switch-to-theme 'zerodark))
-
-(use-package zenburn
-  :disabled t
+  :ensure t
   :if window-system
   :init
   (switch-to-theme 'zenburn)
@@ -217,6 +205,7 @@
                 :style unspecified)))))))
 
 (use-package solarized-theme
+  :ensure t
   :if window-system
   :init
   (setq solarized-use-less-bold t
@@ -235,6 +224,7 @@
 
 (use-package leuven-theme
   :disabled t
+  :ensure t
   :if window-system
   :init (switch-to-theme 'leuven)
   :config
@@ -262,15 +252,6 @@
      '(ansi-color-names-vector
        ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"]))))
 
-(use-package cyberpunk-theme
-  :disabled t
-  :if window-system
-  :config
-  (switch-to-theme 'cyberpunk)
-  (custom-theme-set-faces
-   'cyberpunk
-   '(cursor ((t (:background "cyan"))))))
-
 (use-package linum-mode
   :disabled t
   :commands linum-mode
@@ -285,16 +266,6 @@
                  (fmt (concat "%" (number-to-string (1+ width)) "d")))
             (propertize (format fmt line) 'face 'linum)))))
 
-(use-package fic-mode
-  :commands fic-mode
-  :config
-  (add-hook 'prog-mode-hook 'fic-mode))
-
-(use-package unicode-fonts
-  :disabled t
-  :config
-  (unicode-fonts-setup))
-
 (use-package grep
   :defer t
   :config
@@ -307,6 +278,7 @@
 
 (use-package eshell-git-prompt
   :disabled t
+  :ensure t
   :commands eshell-git-prompt-use-theme)
 
 (use-package eshell
@@ -315,10 +287,12 @@
   (add-hook 'eshell-mode-hook 'eshell-fringe-status-mode))
 
 (use-package exec-path-from-shell
+  :ensure t
   :init (when (memq window-system '(mac ns))
           (exec-path-from-shell-initialize)))
 
 (use-package hungry-delete
+  :ensure t
   :diminish hungry-delete-mode
   :init (global-hungry-delete-mode))
 
@@ -330,26 +304,22 @@
   (add-hook 'html-mode-hook 'hl-line-mode))
 
 (use-package undo-tree
+  :ensure t
   :diminish undo-tree-mode
   :init (global-undo-tree-mode t)
   :bind (:map undo-tree-map
-              ("C-c u" . undo-tree-visualize)
-              ("C-x u" . nil)))
-
-(use-package column-enforce-mode
-  :disabled t
-  :diminish column-enforce-mode
-  :commands column-enforce-mode
-  :init (add-hook 'prog-mode-hook 'column-enforce-mode))
+              ("C-x u" . undo-tree-visualize)))
 
 (use-package markdown-mode
+  :ensure t
   :mode "\\.md'"
   :init
   (setq markdown-command "pandoc -f markdown -t html")
   :config
   (add-hook 'markdown-mode-hook 'flyspell-mode))
 
-(use-package editorconfig)
+(use-package editorconfig
+  :ensure t)
 
 (defun davazp/ffap-nodejs-module (name)
   (unless (or (string-prefix-p "/" name)
@@ -367,6 +337,7 @@
                     (concat base name))))))
 
 (use-package ffap
+  :defer t
   :config
   (setq ffap-machine-p-known 'reject)
   (add-to-list 'ffap-alist '(js-mode . davazp/ffap-nodejs-module) t)
@@ -384,16 +355,6 @@
                   ("lithuanian" "[a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ]" "[^a-zA-ZąčęėįšųūžĄČĘĖĮŠŲŪŽ]" "" nil ("-l" "lt") nil utf-8))
                 ispell-program-name "/usr/local/bin/aspell"))
 
-(use-package paren-face
-  :disabled t
-  :init
-  (global-paren-face-mode -1)
-  :config
-  (setq paren-face-regexp "[\])(\[{};]")
-  (add-to-list 'paren-face-modes 'js2-mode)
-  (add-to-list 'paren-face-modes 'js2-jsx-mode)
-  (add-to-list 'paren-face-modes 'js-mode))
-
 (defun dp/xterm-color-compilation-filter ()
   (let* ((inhibit-read-only t)
          (start compilation-filter-start)
@@ -403,73 +364,49 @@
     (insert (xterm-color-filter input))))
 
 (use-package xterm-color
+  :ensure t
+  :commands (xterm-color-filter)
   :init
-  (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
-  (add-hook 'compilation-filter-hook 'dp/xterm-color-compilation-filter)
+  (add-hook 'comint-preoutput-filter-functions #'xterm-color-filter)
+  (add-hook 'compilation-filter-hook #'dp/xterm-color-compilation-filter)
   (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions)))
 
 (use-package multi-term
+  :ensure t
+  :defer t
   :config
   (define-key term-raw-map "\C-c\C-l" 'term-line-mode)
   (define-key term-raw-map "\C-c\C-k" 'term-char-mode))
 
 (use-package multiple-cursors
+  :ensure t
   :bind (("C-M-." . mc/mark-next-like-this)
          ("C-M-," . mc/skip-to-next-like-this)))
 
 (use-package whitespace-cleanup-mode
+  :ensure t
   :diminish whitespace-cleanup-mode
   :init (global-whitespace-cleanup-mode 1))
 
 (use-package macrostep
+  :ensure t
   :commands macrostep-expand
   :init (bind-key "C-c e" 'macrostep-expand emacs-lisp-mode-map))
-
-(use-package rainbow-delimiters
-  :disabled t
-  :config
-  (remove-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (use-package eldoc
   :diminish eldoc-mode
   :commands eldoc-mode
   :config (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
 
-(use-package saveplace
-  :disabled t
-  :init
-  (setq-default save-place t)
-  (setq save-place-file "~/.emacs.d/data/places"))
-
-(use-package desktop-save-mode
-  :disabled t
-  :init (desktop-save-mode t))
-
-(use-package savehist-mode
-  :disabled t
-  :init (savehist-mode t))
-
 (use-package uniquify
   :config (setq uniquify-buffer-name-style 'forward))
 
 (use-package expand-region
+  :ensure t
   :bind ("C-=" . er/expand-region))
 
-;; (require 'drag-stuff)
-;; (drag-stuff-global-mode t)
-
-;; (use-package swiper
-;;   :ensure t)
-
-;; (use-package ivy
-;;   :ensure t
-;;   :config
-;;   (setq ivy-re-builders-alist '((t . ivy--regex-ignore-order))))
-
-;; (use-package counsel
-;;   :ensure t)
-
 (use-package wrap-region
+  :ensure t
   :diminish wrap-region-mode
   :init (wrap-region-global-mode)
   :config
@@ -478,19 +415,17 @@
       (wrap-region-add-wrapper "`" "`" nil mode)
       (wrap-region-add-wrapper "_" "_" nil mode))))
 
-(use-package paredit
-  :config
-  (add-hook 'emacs-lisp-mode #'paredit-mode))
-
-(use-package smartparens
-  :disabled t
-  :bind ("M-p" . sp-rewrap-sexp))
+(use-package visual-regexp-steroids
+  :ensure t
+  :defer t)
 
 (use-package visual-regexp
+  :ensure t
   :bind ("C-c r" . vr/replace)
-  :init (require 'visual-regexp-steroids))
+  :config (require 'visual-regexp-steroids))
 
 (use-package company
+  :ensure t
   :diminish company-mode
   :init
   (progn
@@ -506,20 +441,22 @@
   :config
   (setq company-idle-delay 0.1))
 
-(use-package auto-complete
-  :disabled t
-  :diminish auto-complete-mode
-  :init (remove-hook 'prog-mode-hook 'auto-complete-mode))
-
 (use-package dictionary
+  :ensure t
   :commands dictionary-search
   :bind ("C-c s" . dictionary-search))
 
 (use-package flx-isearch
+  :ensure t
   :bind (("C-M-s" . flx-isearch-forward)
          ("C-M-r" . flx-isearch-backward)))
 
+(use-package ido-ubiquitous :ensure t :defer t)
+(use-package ido-vertical-mode :ensure t :defer t)
+(use-package flx-ido :ensure t :defer t)
+
 (use-package ido
+  :ensure t
   :init (ido-mode t)
   :config
   (defun dp/ido-keys ()
@@ -542,10 +479,12 @@
         ido-default-buffer-method 'selected-window))
 
 (use-package smex
+  :ensure t
   :bind ("M-x" . smex))
 
-
-(use-package htmlize)
+(use-package htmlize
+  :ensure t
+  :defer t)
 
 (defun eval-and-replace ()
   "Replace the preceding sexp with its evaluated value."
@@ -562,13 +501,15 @@
   :config
   (define-key emacs-lisp-mode-map "\C-c\C-e" #'eval-and-replace))
 
-(use-package elisp-slime-nav-mode
+(use-package elisp-slime-nav
+  :ensure t
   :commands elisp-slime-nav-mode
   :diminish elisp-slime-nav-mode
-  :init
-  (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode))
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'elisp-slime-nav-mode))
 
 (use-package org
+  :ensure t
   :mode ("\\.org$" . org-mode)
   :bind (("C-c C-x C-o" . org-clock-out)
          ("C-c C-x C-j" . org-clock-goto)
@@ -617,6 +558,7 @@ of code to whatever theme I'm using's background"
   (add-hook 'org-export-before-processing-hook 'dp/org-set-source-code-background))
 
 (use-package magit
+  :ensure t
   :bind (("C-x g" . magit-status)
          ("C-x f" . dp/git-find-file))
   :config
@@ -639,6 +581,7 @@ of code to whatever theme I'm using's background"
     (add-hook 'git-commit-mode-hook 'turn-on-flyspell)))
 
 (use-package helm
+  :ensure t
   :bind (("C-c g" . helm-git-grep))
   :config (setq helm-ff-transformer-show-only-basename nil
                 helm-ls-git-show-abs-or-relative 'relative
@@ -646,6 +589,10 @@ of code to whatever theme I'm using's background"
                 helm-recentf-fuzzy-matching t
                 helm-M-x-fuzzy-match t
                 helm-split-window-in-side-p t))
+
+(use-package git-gutter-fringe
+  :ensure t
+  :defer t)
 
 (use-package git-gutter
   :diminish git-gutter-mode
@@ -709,9 +656,9 @@ of code to whatever theme I'm using's background"
     "XXX....."))
 
 (use-package flycheck
+  :ensure t
   :commands (flycheck-mode flycheck-add-mode)
   :config
-
   (flycheck-def-config-file-var flycheck-eslintrc
       javascript-eslint ".eslintrc" :safe #'stringp)
 
@@ -805,17 +752,16 @@ See URL `https://github.com/eslint/eslint'."
        (message (concat "could not read output: " output))))))
 
 (use-package css-eldoc
+  :ensure t
   :commands 'turn-on-css-eldoc
-  :init
+  :config
   (defun css-eldoc-function ()
     (save-excursion
       (back-to-indentation)
       (gethash (symbol-name (symbol-at-point)) css-eldoc-hash-table))))
 
-(defvar css-collapse-ruleset
-  [?\C-r ?\{ return ?\C-= ?\C-c ?r ?  ?+ backspace backspace ?\C-q ?\C-j ?  ?+ return ?  return])
-
 (use-package scss-mode
+  :ensure t
   :mode "\\.scss$"
   :config
   (progn
@@ -825,6 +771,7 @@ See URL `https://github.com/eslint/eslint'."
     (add-to-list 'scss-mode-hook 'turn-on-css-eldoc)))
 
 (use-package sass-mode
+  :ensure t
   :mode "\\.sass$"
   :config
   (progn
@@ -833,6 +780,7 @@ See URL `https://github.com/eslint/eslint'."
     (add-to-list 'sass-mode-hook 'turn-on-css-eldoc)))
 
 (use-package rainbow-mode
+  :ensure t
   :diminish rainbow-mode
   :commands rainbow-mode
   :config (setq css-indent-offset 2)
@@ -842,6 +790,7 @@ See URL `https://github.com/eslint/eslint'."
           (add-hook 'css-mode-hook 'rainbow-mode)))
 
 (use-package dired-details
+  :ensure t
   :commands dired-details-install)
 
 (defun dired-back-to-start-of-files ()
@@ -849,6 +798,7 @@ See URL `https://github.com/eslint/eslint'."
   (backward-char (- (current-column) 2)))
 
 (use-package dired
+  :defer t
   :config
   (require 'dp-ediff)
   (bind-keys :map dired-mode-map
@@ -863,12 +813,8 @@ See URL `https://github.com/eslint/eslint'."
       (after revert-buffer-after-abort activate)
     (revert-buffer)))
 
-(use-package purescript-mode
-  :mode "\\.purs'"
-  :init
-  (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation))
-
-(use-package scala-mode2
+(use-package scala-mode
+  :ensure t
   :mode "\\.scala'"
   :config
   (setq scala-indent:align-parameters nil)
@@ -876,6 +822,7 @@ See URL `https://github.com/eslint/eslint'."
   (add-hook 'scala-mode-hook 'ensime-scala-mode-hook))
 
 (use-package ensime
+  :ensure t
   :commands ensime
   :pin melpa-stable)
 
@@ -884,11 +831,13 @@ See URL `https://github.com/eslint/eslint'."
   :commands subword-mode)
 
 (use-package js2-refactor
+  :ensure t
   :commands js2-refactor-mode
   :config
   (progn (js2r-add-keybindings-with-prefix "C-c C-m")))
 
 (use-package json-mode
+  :ensure t
   :mode "\\.\\(json\\|babelrc\\|jshintrc\\|eslintrc\\|bowerrc\\|json\\.erb\\|watchmanconfig\\)\\'"
   :config
   (add-hook 'json-mode-hook 'flycheck-mode))
@@ -903,10 +852,6 @@ See URL `https://github.com/eslint/eslint'."
               (when arg (json-mode-beautify))
               (buffer-string)))))
 
-(use-package syntax-subword-mode
-  :disabled t
-  :commands syntax-subword-mode)
-
 (require 'flowtype-mode)
 
 (require 'prettier-js)
@@ -916,6 +861,7 @@ See URL `https://github.com/eslint/eslint'."
                               "--trailing-comma=es5"))
 
 (use-package js2-mode
+  :ensure t
   :mode "\\.js\\'"
   :commands js2-mode
   :bind (:map js2-mode-map
@@ -947,10 +893,10 @@ See URL `https://github.com/eslint/eslint'."
   (flycheck-disable-checker 'javascript-jshint)
   (flycheck-add-mode 'javascript-eslint 'js-mode))
 
-(use-package coffee-mode
-  :mode "\\.coffee$")
+(use-package tide :ensure t :defer t)
 
 (use-package typescript-mode
+  :ensure t
   :mode "\\.tsx?$"
   :config
   (setq-default typescript-indent-level 2)
@@ -960,11 +906,12 @@ See URL `https://github.com/eslint/eslint'."
   (add-hook 'typescript-mode-hook #'flycheck-mode))
 
 (use-package skewer-mode
+  :disabled t
   :commands (skewer-run-phantomjs run-skewer skewer-mode)
   :config
   (setq phantomjs-program-name "/usr/local/bin/phantomjs"))
 
-(use-package hippie-expand
+(use-package hippie-exp
   :config
   (setq hippie-expand-try-functions-list
         '(try-complete-file-name-partially
@@ -978,16 +925,8 @@ See URL `https://github.com/eslint/eslint'."
           try-expand-list
           try-expand-line)))
 
-(use-package docker
-  :bind ("C-c d" . dp/docker-machine-select)
-  :config
-  (require 'dp-docker-machine)
-  (dp/docker-machine-select "default"))
-
-;; (use-package docker-tramp)
-
 (use-package elm-mode
-  ;; :quelpa (elm-mode :fetcher file :path "~/Source/elm-mode")
+  :disabled t
   :mode "\\.elm$"
   :config
   (flycheck-elm-setup)
@@ -996,6 +935,7 @@ See URL `https://github.com/eslint/eslint'."
   (add-to-list 'company-backends 'company-elm))
 
 (use-package eproject
+  :ensure t
   :diminish eproject-mode
   :commands define-project-type
   :init
@@ -1013,6 +953,7 @@ See URL `https://github.com/eslint/eslint'."
   (add-hook 'generic-git-project-file-visit-hook 'eproject-set-git-generic-keys))
 
 (use-package php-mode
+  :ensure t
   :mode "\\.\\(php[s345]?\\|inc\\|phtml\\)"
   :config
   (bind-keys
@@ -1038,19 +979,17 @@ See URL `https://github.com/eslint/eslint'."
         nxml-attribute-indent 4))
 
 (use-package utop
+  :ensure t
   :commands utop
   :config
   (setq utop-command "opam config exec -- utop -emacs"))
 
-(setq *opam-prefix*
-      (substring (shell-command-to-string "opam config var prefix 2> /dev/null") 0 -1))
-
-(setq *opam-share*
-      (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
-
 (use-package tuareg
+  :ensure t
   :mode ("\\.ml\\'" . tuareg-mode)
   :config
+  (setq *opam-share*
+        (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
   (add-to-list 'load-path (concat *opam-share* "/emacs/site-lisp"))
   (require 'ocp-indent)
   (ocp-setup-indent)
@@ -1058,29 +997,8 @@ See URL `https://github.com/eslint/eslint'."
   (add-hook 'tuareg-mode-hook #'merlin-mode t)
   (add-hook 'caml-mode-hook #'merlin-mode t))
 
-;; (defun dp/opam-bin (name &optional flags)
-;;   (let ((command (concat *opam-prefix* "/bin/" name)))
-;;     (if flags
-;;         (concat command " " (s-join " " args))
-;;       command)))
-
-;; (defun dp/reason-mode-setup ()
-;;   (setq merlin-default-flags '("-pp" "ocamlmerlin-reason"))
-;;   (add-hook 'before-save-hook 'refmt-before-save)
-;;   (merlin-mode))
-
-;; (defun dp/reason-setup ()
-;;   (add-to-list 'load-path (concat *opam-prefix* "/share/emacs/site-lisp"))
-;;   (setq refmt-command (dp/opam-bin "refmt"))
-;;   (setq merlin-command (dp/opam-bin "ocamlmerlin"))
-;;   (require 'reason-mode)
-;;   (require 'merlin)
-;;   (require 'merlin-company)
-;;   (add-hook 'reason-mode-hook #'dp/reason-mode-setup))
-
-;; (dp/reason-setup)
-
 (use-package mmm-mode
+  :ensure t
   :commands (mmm-mode mmm-mode-on)
   :config
   (require 'mmm-defaults)
@@ -1109,6 +1027,7 @@ See URL `https://github.com/eslint/eslint'."
     (indent-region (point-min) (point-max))))
 
 (use-package yasnippet
+  :ensure t
   :commands (yas-minor-mode yas-global-mode)
   :init
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
@@ -1123,6 +1042,8 @@ See URL `https://github.com/eslint/eslint'."
   :config (add-hook 'log-edit-mode-hook 'flyspell-mode))
 
 (use-package olivetti
+  :disabled t
+  :ensure t
   :commands olivetti-mode
   :init (setq-default olivetti-body-width 120))
 
@@ -1149,16 +1070,22 @@ See URL `https://github.com/eslint/eslint'."
                               file))
                           (ad-get-arg 0)))))
 
-(use-package csv-mode :mode "\\.csv\\'")
+(use-package csv-mode
+  :ensure t
+  :mode "\\.csv\\'")
 
-(use-package yaml-mode :mode "\\.yml\\'")
+(use-package yaml-mode
+  :ensure t
+  :mode "\\.yml\\'")
 
 (use-package command-log-mode
-  :commands (command-log-mode clm/open-command-log-buffer)
+  :ensure t
+  :defer t
   :config
   (setq command-log-mode-auto-show t))
 
 (use-package evil
+  :disabled t
   :ensure t
   :defer t)
 
