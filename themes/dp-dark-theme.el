71784a7dc6210070ce05c1a8a58a6fd472882d1f
Donatas Petrauskas <donatasp@wix.com>
2018-09-25 17:58:19 +0300

    move custom faces to themes

---
+++ themes/dp-dark-theme.el
@@ -0,0 +1,18 @@
+(deftheme dp-dark)
+
+(custom-theme-set-faces
+ 'dp-dark
+ `(default ((t (:background "gray10" :foreground "white"))))
+ `(fringe ((t (:background "gray20"))))
+ `(font-lock-comment-face ((t (:foreground "chocolate" :slant normal))))
+ `(git-gutter:added ((t (:foreground "yellow green" :inherit nil))))
+ `(git-gutter:deleted ((t (:foreground "tomato" :inherit nil))))
+ `(git-gutter:modified ((t (:foreground "orchid" :inherit nil))))
+ `(trailing-whitespace ((t (:background "hot pink"))))
+ `(error ((t (:foreground "tomato" :underline "red"))))
+ `(js2-external-variable ((t (:foreground "gray60"))))
+ `(mode-line ((t (:inherit default :overline "gray60" :box (:line-width 2 :color "gray10")))))
+ `(mode-line-inactive ((t (:inherit default :foreground "gray60" :overline "gray40" :box (:line-width 2 :color "gray10")))))
+ `(cursor ((t (:background "red")))))
+
+(provide-theme 'dp-dark)
---
+++ themes/dp-light-theme.el
@@ -0,0 +1,18 @@
+(deftheme dp-light)
+
+(custom-theme-set-faces
+ 'dp-light
+ `(default ((t (:background "white" :foreground "gray10"))))
+ `(fringe ((t (:background "gray90"))))
+ `(font-lock-comment-face ((t (:foreground "chocolate" :slant normal))))
+ `(git-gutter:added ((t (:foreground "yellow green" :inherit nil))))
+ `(git-gutter:deleted ((t (:foreground "tomato" :inherit nil))))
+ `(git-gutter:modified ((t (:foreground "orchid" :inherit nil))))
+ `(trailing-whitespace ((t (:background "pink"))))
+ `(error ((t (:foreground "tomato" :underline "red"))))
+ `(js2-external-variable ((t (:foreground "gray40"))))
+ `(mode-line ((t (:inherit default :overline "gray60" :box (:line-width 2 :color "white")))))
+ `(mode-line-inactive ((t (:inherit default :foreground "gray70" :overline "gray60" :box (:line-width 2 :color "white")))))
+ `(cursor ((t (:background "red")))))
+
+(provide-theme 'dp-light)
