c3cc7b1e9cdd297dc77eaaf7bca90c170c24b7a8
Donatas Petrauskas <donatasp@wix.com>
2019-11-13 13:42:28 +0200

    Simpler personal themes.

--- themes/dp-dark-theme.el
+++ themes/dp-dark-theme.el
@@ -3,20 +3,13 @@
 (custom-theme-set-faces
  'dp-dark
  `(default ((t (:background "gray10" :foreground "white"))))
- `(variable-pitch ((t (:family "Georgia"))))
- `(fixed-pitch ((t (:family "Courier"))))
- `(org-block ((t (:inherit fixed-pitch))))
- `(org-table ((t (:inherit fixed-pitch))))
- `(fringe ((t (:background "gray20"))))
- `(font-lock-comment-face ((t (:foreground "chocolate" :slant normal))))
+ `(font-lock-comment-face ((t (:foreground "chocolate" :slant italic))))
  `(git-gutter:added ((t (:foreground "yellow green" :inherit nil))))
  `(git-gutter:deleted ((t (:foreground "tomato" :inherit nil))))
  `(git-gutter:modified ((t (:foreground "orchid" :inherit nil))))
  `(trailing-whitespace ((t (:background "hot pink"))))
  `(error ((t (:foreground "tomato" :underline "red"))))
  `(js2-external-variable ((t (:foreground "gray60"))))
- `(mode-line ((t (:inherit default :overline "gray60" :box (:line-width 2 :color "gray10")))))
- `(mode-line-inactive ((t (:inherit default :foreground "gray60" :overline "gray40" :box (:line-width 2 :color "gray10")))))
  `(cursor ((t (:background "red")))))
 
 (provide-theme 'dp-dark)
--- themes/dp-light-theme.el
+++ themes/dp-light-theme.el
@@ -2,19 +2,19 @@
 
 (custom-theme-set-faces
  'dp-light
- `(default ((t (:background "white" :foreground "gray10"))))
+ `(default ((t (:background "gray92" :foreground "gray10"))))
  `(variable-pitch ((t (:family "Georgia"))))
  `(fixed-pitch ((t (:family "Courier"))))
- `(fringe ((t (:background "white"))))
- `(font-lock-comment-face ((t (:foreground "chocolate" :slant normal))))
+ `(fringe ((t (:background "gray92"))))
+ `(font-lock-comment-face ((t (:foreground "chocolate" :slant italic))))
  `(git-gutter:added ((t (:foreground "yellow green" :inherit nil))))
  `(git-gutter:deleted ((t (:foreground "tomato" :inherit nil))))
  `(git-gutter:modified ((t (:foreground "orchid" :inherit nil))))
  `(trailing-whitespace ((t (:background "pink"))))
  `(error ((t (:foreground "tomato" :underline "red"))))
  `(js2-external-variable ((t (:foreground "gray40"))))
- `(mode-line ((t (:inherit default :overline "gray60" :box (:line-width 2 :color "white")))))
- `(mode-line-inactive ((t (:inherit default :foreground "gray70" :overline "gray60" :box (:line-width 2 :color "white")))))
+ ;; `(mode-line ((t (:inherit default :overline "gray60" :box (:line-width 2 :color "white")))))
+ ;; `(mode-line-inactive ((t (:inherit default :foreground "gray70" :overline "gray60" :box (:line-width 2 :color "white")))))
  `(cursor ((t (:background "red")))))
 
 (provide-theme 'dp-light)
