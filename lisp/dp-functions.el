5840c347ff6c37afc52fee93b853418a86930546
Donatas Petrauskas <donatasp@wix.com>
2020-11-10 18:16:55 +0200

    dynamic-cursor-mode

--- lisp/dp-functions.el
+++ lisp/dp-functions.el
@@ -173,15 +173,6 @@ max points of current buffer if there is no selected region."
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
 
-(defun set-cursor-according-to-mode (&optional type)
-  (let ((next-cursor-type
-         (cond
-          (type)
-          (buffer-read-only 'hbar)
-          (t '(bar . 2)))))
-    (unless (equal next-cursor-type cursor-type)
-      (setq cursor-type next-cursor-type))))
-
 (defun download-to-current-buffer (url)
   "Downloads URL into current buffer."
   (interactive "sURL: ")
