45ce84f302ec0c218ab8772edd604792138c1e3d
Donatas Petrauskas <donatas.petr@gmail.com>
2023-01-05 23:23:03 +0200

    snapshot

--- lisp/setup-dired.el
+++ lisp/setup-dired.el
@@ -1,10 +1,17 @@
 ;; -*- lexical-binding: t; -*-
 
+(defun ffplay (file)
+  "Play file under point."
+  (interactive (list (dired-copy-filename-as-kill)))
+  (async-shell-command (format "ffplay -infbuf \"%s\"" file)
+                       (format "*ffplay %s*" file)))
+
 (use-package dired
   :hook ((dired-mode . dired-hide-details-mode)
          (dired-mode . hl-line-mode))
   :bind (:map dired-mode-map
-              ("i" . dired-subtree-insert)))
+              ("i" . dired-subtree-insert)
+              ("r" . dired-subtree-remove)))
 
 (use-package dired-subtree
   :commands (dired-subtree-insert))
