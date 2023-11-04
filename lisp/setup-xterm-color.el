add2199199d780c870cf9dbeecb45103f960ba07
Donatas Petrauskas <donatas.petr@gmail.com>
2023-11-04 13:48:14 +0200

    ignore some ansi escape codes

--- lisp/setup-xterm-color.el
+++ lisp/setup-xterm-color.el
@@ -1,3 +1,18 @@
+;; Stripping non-color ANSI escape codes.
+;;
+;; from https://emacs.stackexchange.com/a/38531
+(defvar ansi-ignored-escape-codes
+  (rx (or
+       ;; icon name escape sequences
+       (regexp "\033\\][0-2];.*?\007")
+       ;; non-SGR CSI escape sequences
+       (regexp "\033\\[\\??[0-9;]*[^0-9;m]")
+       ;; noop
+       (regexp "\012\033\\[2K\033\\[1F"))))
+
+(defun ansi-escape-codes-filter (string)
+  (replace-regexp-in-string ansi-ignored-escape-codes "" string))
+
 (defun xterm-color-compilation-filter ()
   (let* ((inhibit-read-only t)
          (start compilation-filter-start)
@@ -8,10 +23,11 @@
 
 ;; Nice colors for tools that produce color escape codes.
 (use-package xterm-color
-  :commands xterm-color-filter
+  :after commint
   :hook ((compilation-filter . xterm-color-compilation-filter))
   :config
-  (add-hook 'comint-preoutput-filter-functions #'xterm-color-filter)
-  (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions)))
+  (add-hook 'comint-preoutput-filter-functions #'xterm-color-filter 0)
+  (add-hook 'comint-preoutput-filter-functions #'ansi-escape-codes-filter 1)
+  (remove-hook 'comint-output-filter-functions 'ansi-color-process-output))
 
 (provide 'setup-xterm-color)
