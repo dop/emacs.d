0c9ac11c2098225ab7400d6d20bfee0d5a635f60
Donatas Petrauskas <donatasp@wix.com>
2026-02-06 12:05:56 +0200

    confirm kill async shell command

--- custom.el
+++ custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(async-shell-command-buffer 'confirm-kill-process)
  '(backup-by-copying t)
  '(backup-directory-alist '(("." . "~/.emacs.d/backups")))
  '(blink-cursor-blinks 0)
